8405a68fd0ae099c065bd51c6cb12e95b84b1add
Nícolas Ribeiro <53842783+nicolasrls@users.noreply.github.com>
2019-11-20 23:13:49 -0300

    Projeto Final - 90% concluído -
    FALTANDO MELHORIA NO DESIGN E CORREÇÃO DE BUGS. DIA PARA POSSÍVEL AJUSTE -> 22/11/2019

---
+++ SCOP/mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QTimer>
+#include <QMessageBox>
+#include <QFile>
+#include <QInputDialog>
+#include <QTextStream>
+#include "cadastro.h"
+#include "objeto.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void inserirNaTabela(Cadastro a, int linha);
+    void on_btncadastrar_clicked();
+    void on_ordmenorcodigo_clicked();
+    void on_ordmaiorvalor_clicked();
+    void on_ordpeso_clicked();
+
+    void on_actionAbrir_triggered();
+
+    void on_actionSalvar_triggered();
+
+    void on_actionSair_triggered();
+
+
+
+    void on_tabela_cellDoubleClicked(int row, int column);
+
+    void on_btnAjuda_clicked();
+
+    void on_actionExcluir_triggered();
+
+    void on_actionLimpar_Tabela_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
---
+++ SCOP/objeto.h
@@ -0,0 +1,40 @@
+#ifndef OBJETO_H
+#define OBJETO_H
+#include "cadastro.h"
+#include <QVector>
+#include <algorithm>
+#include <QFile>
+#include <QFileDialog>
+#include <QStringList>
+#include <QMessageBox>
+
+
+class Objeto
+{
+
+private:
+    QVector <Cadastro> objetos;
+
+public:
+    Objeto();
+    void inserirObjeto(Cadastro a);
+    void ordenarPorCodigo();
+    void ordenarPorValor();
+    void ordenarPorPeso();
+    void salvarArquivo(QString arquivo);
+    bool carregarArquivo(QString arquivo);
+    int size();
+    Cadastro operator[](int indice);
+    bool jaSalvo(QFile arquivo);
+    void eraseObj(QString txt);
+    void apagarObj(QString txt);
+    void eraseCi(QString txt);
+    void eraseData(QString txt);
+    void eraseValor(QString txt);
+    void erasePeso(QString txt);
+    void eraseStatus(QString txt);
+    void eraseDestino(QString txt);
+
+};
+
+#endif // OBJETO_H
